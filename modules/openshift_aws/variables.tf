f559fe85e8be71f5314d069bc69afa4abcac5c6b
Lakshmi Narasimhan <lakshmi@lakshminp.com>
2018-11-08 08:26:21 -0500

    VPC, security groups and nodes.

--- modules/openshift_aws/variables.tf
+++ modules/openshift_aws/variables.tf
@@ -1,3 +1,8 @@
+variable "key_name" {
+  default = "openshift"
+  description = "The name of the key to user for ssh access, e.g: consul-cluster"
+}
+
 variable "public_key_path" {
   default = "~/.ssh/tf.pub"
   description = "The local public key path, e.g. ~/.ssh/id_rsa.pub"
@@ -12,6 +17,30 @@ variable "region" {
   default = "us-east-1"
   description = "The AWS region where the cluster will be spun."
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "subnetaz" {
+  type = "map"
+
+  default = {
+    us-east-1 = "us-east-1a"
+    us-east-2 = "us-east-2a"
+    us-west-1 = "us-west-1a"
+    us-west-2 = "us-west-2a"
+    eu-west-1 = "eu-west-1a"
+    eu-west-2 = "eu-west-2a"
+    eu-central-1 = "eu-central-1a"
+    ap-southeast-1 = "ap-southeast-1a"
+  }
+}
+
 
 variable "master_size" {
   default = "4gb"
