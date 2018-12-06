f229bfd4b13f216d5f2be0e4b9e844343fe7a6ff
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2018-12-06 05:23:16 -0500

    Add empty vars file.

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "public_key" {}
+
+variable "region" {}
+
+variable "master_size" {}
+
+variable "node_size" {}
+
+variable "nodes_count" {}
+
+variable "volume_size" {}
+
+variable "domain" {}
+
+variable "key_name" {}
