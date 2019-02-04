b0f6cc0f5bc375c86b1f57f20531b021a635d73d
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2019-02-04 11:20:17 -0500

    Change volume names with cluster id context.

--- openshift_digitalocean/volumes.tf
+++ openshift_digitalocean/volumes.tf
@@ -2,13 +2,13 @@
 
 resource "digitalocean_volume" "master_volume" {
   region = "${var.region}"
-  name   = "master-glusterfs"
+  name   = "${format("shapeblock-%s-master-glusterfs", var.domain)}"
   size   = "${var.volume_size}"
 }
 
 resource "digitalocean_volume" "node_volumes" {
   region = "${var.region}"
-  name   = "${format("%s%02d-glusterfs", var.node_prefix, count.index + 1)}"
+  name   = "${format("shapeblock-%s-%s%02d-glusterfs", var.domain, var.node_prefix, count.index + 1)}"
   size   = "${var.volume_size}"
   count  = "${var.nodes_count}"
 }
